cba21ad13e15a745051ab3ba8b3b5ca71946a055
TaKaQod <takaqod@gmail.com>
2018-01-10 22:56:39 +0100

    post some form data

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.use(bodyParser.json());
+
+app.post('/file-upload', (req, res) => {
+  const { stuff } = req.body;
+  if (typeof stuff === 'string' && stuff) {
+    console.log(`Upload: ${stuff}`);
+    res.send('ok!');
+  }
+  else {
+    res.status(400).send('bad bad BAAAD!');
+  }
+});
+
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('Super server listening on port 3000!');
 });
